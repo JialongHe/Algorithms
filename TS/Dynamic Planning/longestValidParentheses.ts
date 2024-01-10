902bc38fcd8c6cfe708cdaadcdab123d3fd02470
JoH <47890081+JoH-47@users.noreply.github.com>
2024-01-10 22:27:33 +0100

    32. Longest Valid Parentheses Stack & DP

--- TS/Dynamic Planning/longestValidParentheses.ts
+++ TS/Dynamic Planning/longestValidParentheses.ts
@@ -1,18 +1,42 @@
 // 32. Longest Valid Parentheses
 // https://leetcode.com/problems/longest-valid-parentheses/description/
 
-// 
+// Stack
+function longestValidParentheses2(s: string): number {
+    let stack: number[] = [-1];
+    let maxLen: number = 0;
+
+    for (let i: number = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            stack.push(i);
+        } else if (stack.length === 1) {
+            stack[0] = i;
+        } else {
+            stack.pop();
+            maxLen = Math.max(maxLen, i - stack[stack.length - 1]);
+        }
+        // console.log(`stack: ${stack}, i: ${i}, s[i]: ${s[i]} maxLen: ${maxLen}`);
+    }
+
+    return maxLen;
+};
+
+// DP
 function longestValidParentheses(s: string): number {
     let maxLen: number = 0;
-    let l: number = 0;
-    let r: number = 0;
-    let lp: number = 0;
-    let rp: number = 0;
+    let dp: number[] = new Array(s.length).fill(0);
 
-    while (r < s.length) {
-        
-
-        r++;
+    for (let i = 1; i < s.length; i++) {
+        if (s[i - 1] === '(' && s[i] === ')') {
+            dp[i] = 2 + (dp[i - 2] || 0);
+            maxLen = Math.max(dp[i], maxLen);
+        }
+        if (s[i - 1] === ')' &&
+            s[i] === ')' && 
+            s[i - dp[i - 1] - 1] === '(') {
+            dp[i] = 2 + dp[i - 1] + (dp[i - dp[i - 1] - 2] || 0);
+            maxLen = Math.max(dp[i], maxLen);
+        }
     }
 
     return maxLen;
@@ -20,4 +44,5 @@ function longestValidParentheses(s: string): number {
 
 console.log(longestValidParentheses("(()"));
 console.log(longestValidParentheses(")()())"));
+console.log(longestValidParentheses("()(())"))
 console.log(longestValidParentheses(""));
