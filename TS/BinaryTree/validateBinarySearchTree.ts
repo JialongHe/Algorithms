bfc717c302bcb3354d2c271d8cf45c4bb0a8fd93
JoH <47890081+JoH-47@users.noreply.github.com>
2024-03-12 11:34:33 +0100

    98. Validate Binary Search Tree - Morris Traversal

---
+++ TS/BinaryTree/validateBinarySearchTree.ts
@@ -0,0 +1,64 @@
+// 98. Validate Binary Search Tree
+// https://leetcode.com/problems/validate-binary-search-tree/description/?envType=study-plan-v2&envId=top-interview-150
+
+// Inorder Traversal
+function isValidBST3(root: TreeNode | null): boolean {
+    const temp: number[] = [];
+
+    const inorderTraversal = (root: TreeNode | null): void => {
+        if (root !== null) {
+            inorderTraversal(root.left);
+            temp.push(root.val);
+            inorderTraversal(root.right);
+        }
+    }
+
+    inorderTraversal(root);
+
+    for (let i: number = 1; i < temp.length; i++) {
+        if (temp[i - 1] >= temp[i]) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+// Recursive
+function isValidBST2(root: TreeNode | null, minValue: number = Number.NEGATIVE_INFINITY, maxValue: number = Number.POSITIVE_INFINITY): boolean {
+    if (root === null) {
+        return true;
+    }
+
+    if (minValue >= root.val || maxValue <= root.val) {
+        return false;
+    }
+
+    return isValidBST2(root.left, minValue, root.val) && isValidBST2(root.right, root.val, maxValue);
+};
+
+// Morris Traversal (Space Optimized)
+function isValidBST(root: TreeNode | null): boolean {
+    let cur: TreeNode | null = root;
+
+    while (cur !== null) {
+        if (cur.left !== null) {
+            const left: TreeNode = cur.left;
+            let rightmost: TreeNode = left;
+            while(rightmost.right !== null)
+                rightmost = rightmost.right;
+
+            cur.left = null;
+            rightmost.right  = cur;
+            cur = left;
+        } else {
+            let next: TreeNode | null = cur.right;
+            if (next !== null && cur.val >= next.val) {
+                return false;
+            }
+            cur = next;
+        }
+    }
+
+    return true;    
+};
