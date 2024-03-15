4d1ccae912bfd424bbb769fec6d8f541cfb641df
JoH <47890081+JoH-47@users.noreply.github.com>
2024-03-15 09:55:36 +0100

    105. Construct Binary Tree from Preorder and Inorder Traversal - Recursion

--- TS/BinaryTree/constructBinaryTreeFromPreorderAndInorderTraversal.ts
+++ TS/BinaryTree/constructBinaryTreeFromPreorderAndInorderTraversal.ts
@@ -1,6 +1,22 @@
 // 105. Construct Binary Tree from Preorder and Inorder Traversal
 // https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/?envType=study-plan-v2&envId=top-interview-150
 
+// Recursion
+function buildTree2(preorder: number[], inorder: number[]): TreeNode | null {
+	if (!inorder.length) return null;
+
+	const root: number = preorder.shift()!;
+	const inOrderRootIndex: number = inorder.findIndex((val) => val === root);
+	const inOrderLeftBranch: number[] = inorder.slice(0, inOrderRootIndex);
+	const inOrderRightBranch: number[] = inorder.slice(inOrderRootIndex + 1);
+
+	const tree: TreeNode = new TreeNode(root);
+	tree.left = buildTree2(preorder, inOrderLeftBranch);
+	tree.right = buildTree2(preorder, inOrderRightBranch);
+
+	return tree;
+}
+
 // Morris Traversal
 function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
     let root: TreeNode | null = null;
