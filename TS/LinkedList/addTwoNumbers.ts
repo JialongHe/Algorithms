8b335ba13f2b0e8bd7eda7a48644368a6c202b82
JoH <47890081+JoH-47@users.noreply.github.com>
2024-03-21 00:38:31 +0100

    2. Add Two Numbers

---
+++ TS/LinkedList/addTwoNumbers.ts
@@ -0,0 +1,26 @@
+// 2. Add Two Numbers
+// https://leetcode.com/problems/add-two-numbers/description/?envType=study-plan-v2&envId=top-interview-150
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    let head: ListNode = new ListNode();
+    let cur: ListNode = head;
+    let carry: number = 0;
+
+    while (l1 || l2) {
+        let x: number = l1?.val || 0;
+        let y: number = l2?.val || 0;
+        let sum: number = x + y + carry;
+        carry = Math.floor(sum / 10);
+        sum = sum % 10;
+
+        cur.next = new ListNode(sum);
+        cur = cur.next;
+        l1 = l1?.next || null;
+        l2 = l2?.next || null;
+    }
+
+    if (carry > 0)
+        cur.next = new ListNode(carry);
+
+    return head.next;
+};
