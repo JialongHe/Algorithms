41b88936ae6dbea5791636c77726bacdad3eb63b
JoH <47890081+JoH-47@users.noreply.github.com>
2024-05-16 19:52:09 +0200

    72. Edit Distance - Dynamic Planning

--- TS/Dynamic Planning/editDistance.ts
+++ TS/Dynamic Planning/editDistance.ts
@@ -2,7 +2,7 @@
 // https://leetcode.com/problems/edit-distance/description/?envType=study-plan-v2&envId=top-interview-150
 
 // DP
-function minDistance(word1: string, word2: string): number {
+function minDistance2(word1: string, word2: string): number {
     let m: number = word1.length;
     let n: number = word2.length;
 
@@ -15,7 +15,6 @@ function minDistance(word1: string, word2: string): number {
     for (let j: number = 1; j <= n; j++) {
         dp[0][j] = j;
     }
-    console.log(dp)
 
     for (let i: number = 1; i <= m; i++) {
         for (let j: number = 1; j <= n; j++ ) {
@@ -26,12 +25,35 @@ function minDistance(word1: string, word2: string): number {
             }
         }
     }
-    console.log(dp)
     return dp[m][n];
 };
 
-// TODO: Space Optimized
+// Space Optimized
+function minDistance(word1: string, word2: string): number {
+    let m: number = word1.length;
+    let n: number = word2.length;
+    let pre: number;
 
+    let cur: number[] = Array(n + 1).fill(0);
+    
+    for (let j: number = 1; j <= n; j++) {
+        cur[j] = j;
+    }
+    for (let i: number = 1; i <= m; i++) {
+        pre = cur[0];
+        cur[0] = i;
+        for (let j: number = 1; j <= n; j++ ) {
+            const temp: number = cur[j];
+            if (word1[i - 1] === word2[j - 1]) {
+                cur[j] = pre;
+            } else {
+                cur[j] = Math.min(pre, cur[j - 1], cur[j]) + 1;
+            }
+            pre = temp;
+        }
+    }
+    return cur[n];
+};
 
 console.log(minDistance("horse", "ros"));
 console.log(minDistance("intention", "execution"));
